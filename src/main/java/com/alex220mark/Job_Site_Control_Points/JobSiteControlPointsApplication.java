0751040e6dd7d951c2ae2272259bae329aaf6895
unknown <alex220mark@gmail.com>
2022-08-12 13:52:16 +0200

    batman

---
+++ src/main/java/com/alex220mark/Job_Site_Control_Points/JobSiteControlPointsApplication.java
@@ -0,0 +1,13 @@
+package com.alex220mark.Job_Site_Control_Points;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSiteControlPointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSiteControlPointsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex220mark/Job_Site_Control_Points/JobSiteControlPointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex220mark.Job_Site_Control_Points;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSiteControlPointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
